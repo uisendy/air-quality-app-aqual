e9527bf114fdd6b26c80fc613d25cc39ec6d84f5
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-05 17:03:04 +0100

    [f-Route]- added: handleSearch Function

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,36 +1,15 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import SearchBar from '../components/SearchBar';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HiSearchCircle } from 'react-icons/hi';
 
 import { fetchLocation } from '../redux/searchLocationsSlice';
 
 const HomePage = () => {
-  const dispatch = useDispatch();
-  const [search, setSearch] = useState('');
-  const [addRequestStatus, setAddRequestStatus] = useState('idle');
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    if (addRequestStatus === 'idle') {
-      try {
-        setAddRequestStatus('pending');
-        dispatch(fetchLocation(search)).unwrap();
-        setSearch('');
-      } catch (err) {
-        throw new Error(err);
-      } finally {
-        setAddRequestStatus('idle');
-      }
-    }
-  };
-
   return (
-    <div className="w-[420px]">
-      <SearchBar
-        handleSearch={handleSearch}
-        search={search}
-        setSearch={setSearch}
-      />
+    <div className=" max-w-[440px] w-[100vw] min-h-[100vh] mx-auto bg-gray-100">
+      <Link to={'search'}>
+        <HiSearchCircle />
+      </Link>
     </div>
   );
 };
