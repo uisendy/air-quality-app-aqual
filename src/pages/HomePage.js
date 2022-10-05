8d5d7dfc2df86e5f7f02bfae9f5e056d93218ea5
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-05 10:47:46 +0100

    [f-API]- update: Add: fetch dispatch call. Local States, and Search Component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import SearchBar from '../components/SearchBar';
+
+import { fetchLocation } from '../redux/searchLocationsSlice';
+
+const HomePage = () => {
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState('');
+  const [addRequestStatus, setAddRequestStatus] = useState('idle');
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (addRequestStatus === 'idle') {
+      try {
+        setAddRequestStatus('pending');
+        dispatch(fetchLocation(search)).unwrap();
+        setSearch('');
+      } catch (err) {
+        throw new Error(err);
+      } finally {
+        setAddRequestStatus('idle');
+      }
+    }
+  };
+
+  return (
+    <div className="w-[420px]">
+      <SearchBar
+        handleSearch={handleSearch}
+        search={search}
+        setSearch={setSearch}
+      />
+    </div>
+  );
+};
+
+export default HomePage;
