b6599698c97a6be905e76038ce9c8b5419619f46
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-05 16:58:48 +0100

    [f-Route]- update:  constructed AirQualityURL

--- src/data/apiData.js
+++ src/data/apiData.js
@@ -1,6 +1,10 @@
 const APP_ID = '7771f6d8da515cc8943b9f6d81e4afc8';
-const BASE_URL = 'http://api.openweathermap.org/geo/1.0/direct?';
+const AP_BASE_URL = 'http://api.openweathermap.org/data/2.5/air_pollution?';
+const GEO_BASE_URL = 'http://api.openweathermap.org/geo/1.0/direct?';
 const limit = 5;
 
 export const geoURL = (location) =>
-  `${BASE_URL}q=${location}&limit=${limit}&appid=${APP_ID}`;
+  `${GEO_BASE_URL}q=${location}&limit=${limit}&appid=${APP_ID}`;
+
+export const airQualityURL = (lat, lon) =>
+  `${AP_BASE_URL}lat=${lat}&lon=${lon}&appid=${APP_ID}`;
