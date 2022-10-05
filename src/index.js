fe3226616a73a0ab1bc808ff592074c88e6b775b
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-05 11:59:43 +0100

    [f-Route]- Add: Add routing. add paths

--- src/index.js
+++ src/index.js
@@ -4,10 +4,30 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: 'about',
+    element: <div>About</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>,
 );
