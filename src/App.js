b3f485047ec105900418e7438299ee8557b20c75
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-05 10:43:54 +0100

    [f-API]- Add: Homepage to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import DetailsSummary from './components/DetailsSummary';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
+      <HomePage />
       <DetailsSummary />
     </div>
   );
