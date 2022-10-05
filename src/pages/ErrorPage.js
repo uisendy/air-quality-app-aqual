14b40ad3809edda598c5b025e2bcae2d3d9dfefd
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-05 11:58:14 +0100

    [f-Route]- Add: Error Page and Routing to Home page

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useRouteError, Link } from 'react-router-dom';
+const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <div
+      id="error-page"
+      className=" w-[440px] flex flex-col justify-center h-screen items-center mx-auto bg-gray-300"
+    >
+      <div className="w-[100vw] flex flex-col justify-center h-full items-center gap-5">
+        <h1 className=" text-3xl">Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+        <Link to={'/'}>
+          <button class="block uppercase mx-auto shadow bg-green-800 hover:bg-green-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded">
+            Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
