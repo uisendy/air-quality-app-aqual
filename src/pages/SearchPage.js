055eeef541212f4016f0b0c67c79b25facd63a78
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-05 17:05:27 +0100

    [f-Route]- added: HandleSearch function

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -8,7 +8,20 @@ const SearchPage = () => {
   const dispatch = useDispatch();
   const [search, setSearch] = useState('');
   const [addRequestStatus, setAddRequestStatus] = useState('idle');
-
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (addRequestStatus === 'idle') {
+      try {
+        setAddRequestStatus('pending');
+        dispatch(fetchLocation(search)).unwrap();
+      } catch (err) {
+        throw new Error(err);
+      } finally {
+        setAddRequestStatus('idle');
+        setSearch('');
+      }
+    }
+  };
   return (
     <div className=" relative max-w-[440px] w-[100vw] min-h-[100vh] mx-auto bg-gray-100">
       <SearchBar
