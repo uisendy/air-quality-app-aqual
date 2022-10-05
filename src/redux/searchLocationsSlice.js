1ca19aec870dcdaabd12f4f0ff87340225ac1795
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-05 10:48:30 +0100

    [f-API]- update: Add: CreateAsyncThunk to fetch locations

--- src/redux/searchLocationsSlice.js
+++ src/redux/searchLocationsSlice.js
@@ -1,17 +1,60 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { nanoid, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { geoURL } from '../data/apiData';
 
 const initialState = {
-  locations: { location: 'Abuja' },
+  locations: [],
   status: 'idle',
   error: 'null',
 };
+
+export const fetchLocation = createAsyncThunk(
+  'locations/fetchLocation',
+  async (location) => {
+    try {
+      const response = await axios.get(geoURL(location));
+      return response.data;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+);
 
 const searchLocationsSlice = createSlice({
   name: 'locations',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchLocation.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchLocation.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        const loadedData = action.payload.map((payload) => {
+          const { name, lat, lon, country, state } = payload;
+          return {
+            id: nanoid(),
+            name,
+            lat,
+            lon,
+            country,
+            state,
+            favorite: false,
+          };
+        });
+
+        state.locations = [...loadedData];
+      })
+      .addCase(fetchLocation.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
-export const getSearchLocations = (state) => state.location.location;
+export const getSearchLocations = (state) => state.locations.locations;
+export const getStatus = (state) => state.locations.status;
+export const getError = (state) => state.locations.error;
 
 export default searchLocationsSlice.reducer;
