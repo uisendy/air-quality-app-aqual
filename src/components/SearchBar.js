ea7639b3df81e819a1f777170a111e417536089c
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-05 16:56:27 +0100

    [f-Route]- update:  code cleaning

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import SearchResults from './SearchResults';
 
 const SearchBar = ({ search, setSearch, handleSearch }) => (
-  <div className=" mx-auto max-w-md ">
+  <div className="pt-10 mx-auto max-w-md ">
     <form
       action=""
       onSubmit={(e) => handleSearch(e)}
       className="relative mx-auto w-max"
     >
       <input
-        className="peer cursor-pointer relative z-10 h-12 w-12 rounded-full border bg-transparent pl-12 outline-none focus:w-full focus:cursor-text focus:border-lime-300 focus:pl-16 focus:pr-4"
+        className="peer cursor-pointer relative z-10 h-12 rounded-full border bg-transparent pl-12 outline-none w-full focus:cursor-text focus:border-black focus:pl-16 focus:pr-4"
         type="search"
         name="search"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         placeholder="Search"
+        autoFocus
       />
 
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className=" absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-gray-500 px-3.5 peer-focus:border-lime-300 peer-focus:stroke-lime-500"
+        className=" absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-gray-500 px-3.5 peer-focus:border-black peer-focus:stroke-black"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
@@ -31,7 +31,6 @@ const SearchBar = ({ search, setSearch, handleSearch }) => (
           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
         />
       </svg>
-      <SearchResults />
     </form>
   </div>
 );
