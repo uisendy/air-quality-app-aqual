6e0d6194fd7e97d8c4ec49b8ebd066d3fa9de677
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-06 13:23:16 +0100

    [f-Route]- Add: MomentJs to Update Time automatically

--- src/components/Forcast.js
+++ src/components/Forcast.js
@@ -11,9 +11,10 @@ import { handleBorderColorMap } from '../data/apiData';
 import { Bar } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 import { CategoryScale } from 'chart.js';
-Chart.register(CategoryScale);
+import moment from 'moment';
 
 const Forcast = ({ airQuality }) => {
+  Chart.register(CategoryScale);
   const dispatch = useDispatch();
 
   const forcastAq = useSelector(getforcastAq);
@@ -41,47 +42,60 @@ const Forcast = ({ airQuality }) => {
     content = <p className=" text-gray-400 font-Roboto">Loading...</p>;
   } else if (status === 'succeeded') {
     content = (
-      <div className="">
-        <h3 className="text-xl font-medium pb-1 pt-3 text-center ">Forcast</h3>
-        <p>Time</p>
-        <Bar
-          data={{
-            labels: timeLabel,
-            datasets: [
-              {
-                label: 'Time',
-                data: aqiData,
-                borderColor: borderColor,
-                backgroundColor: color,
-                borderWidth: 2,
-                borderRadius: 20,
-                borderSkipped: false,
-              },
-            ],
-          }}
-          options={{
-            responsive: true,
-            scales: {
-              x: {
-                grid: {
-                  display: false,
+      <div className=" bg-white rounded-2xl drop-shadow-sm mt-3 py-4 font-Roboto">
+        <div className="flex justify-between pb-3 border-b border-solid border-gray-300 px-3">
+          <h3 className="text-l font-medium text-gray-600 ">Forcast</h3>
+          <small className=" font-light text-gray-400">
+            {' '}
+            Updated:
+            {moment(new Date()).startOf('hours').fromNow()}
+          </small>
+        </div>
+        <div className="pt-2 px-2">
+          <Bar
+            data={{
+              labels: timeLabel,
+              datasets: [
+                {
+                  label: 'time',
+                  data: aqiData,
+                  borderColor: borderColor,
+                  backgroundColor: color,
+                  borderWidth: 2,
+                  borderRadius: 20,
+                  borderSkipped: false,
+                },
+              ],
+            }}
+            options={{
+              responsive: true,
+              scales: {
+                x: {
+                  grid: {
+                    display: false,
+                  },
+                  ticks: {
+                    stepSize: 6,
+                  },
+                },
+                y: {
+                  grid: {
+                    display: false,
+                  },
+                  min: 0,
+                  max: 5,
                 },
               },
-              y: {
-                grid: {
-                  display: false,
+              plugins: {
+                legend: {
+                  position: 'none',
                 },
               },
-            },
-            plugins: {
-              legend: {
-                position: 'none',
-              },
-            },
-          }}
-          height={300}
-          width={380}
-        />
+            }}
+            height={200}
+            width={350}
+          />
+        </div>
       </div>
     );
   } else if (status === 'failed') {
