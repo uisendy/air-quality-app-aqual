dfa1deaaf8871e518dc43f3a6475c4235c6ad2fa
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-06 11:48:26 +0100

    [f-Route]- Handle Dispatch to fetch Data

---
+++ src/components/Forcast.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  getforcastAq,
+  getError,
+  getStatus,
+  fetchForcastAq,
+} from '../redux/forcastAqSlice';
+import { handleColorMap } from '../data/apiData';
+import { handleBorderColorMap } from '../data/apiData';
+import { Bar } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import { CategoryScale } from 'chart.js';
+Chart.register(CategoryScale);
+
+const Forcast = ({ airQuality }) => {
+  const dispatch = useDispatch();
+
+  const forcastAq = useSelector(getforcastAq);
+  const status = useSelector(getStatus);
+  const error = useSelector(getError);
+
+  const aqiData = [];
+  const timeLabel = [];
+  forcastAq?.map(
+    (data) => (aqiData.push(data.main?.aqi), timeLabel.push(data.time)),
+  );
+
+  const color = handleColorMap(aqiData);
+  const borderColor = handleBorderColorMap(aqiData);
+
+  useEffect(() => {
+    if (status === 'idle' && Object.keys(airQuality).length) {
+      dispatch(fetchForcastAq(airQuality.location));
+    }
+  }, [airQuality]);
+
+  return <div>{content}</div>;
+};
+
+export default Forcast;
