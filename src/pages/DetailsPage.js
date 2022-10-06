85941649a6633288f4a3ae2531948e4845993c01
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-06 13:24:55 +0100

    [f-Route]- Add: Add useSelector and detailsSummary to UI

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { IoIosClose } from 'react-icons/io';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { getAirQuality, getError, getStatus } from '../redux/airQualitySlice';
+import DetailsSummary from '../components/DetailsSummary';
+import Forcast from '../components/Forcast';
 
 const DetailsPage = () => {
+  const airQuality = useSelector(getAirQuality);
+  const status = useSelector(getStatus);
+  const error = useSelector(getError);
+
   return (
-    <div>
+    <div className="relative max-w-[440px] w-[100vw] min-h-[100vh] mx-auto bg-gray-100">
       <Link to={'/'}>
-        <AiOutlineCloseCircle />
+        <div className="absolute font-bold top-4 left-4 text-black text-2xl border border-solid border-gray-300 rounded-full p-1">
+          <IoIosClose />
+        </div>
       </Link>
-      <h2>New York</h2>
+      <div className="px-5 py-16">
+        <DetailsSummary airQuality={airQuality} status={status} error={error} />
+        <Forcast airQuality={airQuality} />
+      </div>
     </div>
   );
 };
