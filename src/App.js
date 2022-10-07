9296ee0afbdf6c87129acaa0e2a5af8cb3481edf
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-07 21:09:04 +0100

    [f-UI]- Update: Linters Check and code Cleaning

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="max-w-[440px] w-[100vw] min-h-[100vh] mx-auto">
+    <div className="max-w-[440px] w-[100vw] min-h-[100vh] mx-auto bg-gray-100">
       <HomePage />
     </div>
   );
