34fa5c6f564c9038302331f077babf41005066a5
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-07 17:32:00 +0100

    [f-UI]- Update: Code Cleaning

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import { getAirQuality, getError, getStatus } from '../redux/airQualitySlice';
 import DetailsSummary from '../components/DetailsSummary';
 import Forcast from '../components/Forcast';
+import ApComponents from '../components/ApComponents';
 
 const DetailsPage = () => {
   const airQuality = useSelector(getAirQuality);
@@ -13,14 +14,20 @@ const DetailsPage = () => {
 
   return (
     <div className="relative max-w-[440px] w-[100vw] min-h-[100vh] mx-auto bg-gray-100">
-      <Link to={'/'}>
+      <Link to="/">
         <div className="absolute font-bold top-4 left-4 text-black text-2xl border border-solid border-gray-300 rounded-full p-1">
           <IoIosClose />
         </div>
       </Link>
       <div className="px-5 py-16">
-        <DetailsSummary airQuality={airQuality} status={status} error={error} />
+        <DetailsSummary
+          airQuality={airQuality}
+          status={status}
+          error={error}
+          summary
+        />
         <Forcast airQuality={airQuality} />
+        <ApComponents airQuality={airQuality} />
       </div>
     </div>
   );
