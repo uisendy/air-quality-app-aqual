7f520a987db51f4495dabb84731289f3ef59a70c
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-07 17:35:25 +0100

    [f-UI]- Update: Code Cleaning and refactoring

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
-import { createRoot } from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from 'react-router-dom';
 import ErrorPage from './pages/ErrorPage';
 import SearchPage from './pages/SearchPage';
 import DetailsPage from './pages/DetailsPage';
