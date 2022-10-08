a7d6727868ebe4eb1e90f7450f160c52097b5a41
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-08 09:32:27 +0100

    [f-Test]- Add: search icon that routes to searchpage

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -6,6 +6,7 @@ import { getAirQuality, getError, getStatus } from '../redux/airQualitySlice';
 import DetailsSummary from '../components/DetailsSummary';
 import Forcast from '../components/Forcast';
 import ApComponents from '../components/ApComponents';
+import { HiSearchCircle } from 'react-icons/hi';
 
 const DetailsPage = () => {
   const airQuality = useSelector(getAirQuality);
@@ -14,18 +15,18 @@ const DetailsPage = () => {
 
   return (
     <div className="relative max-w-[440px] w-[100vw] min-h-[100vh] mx-auto bg-gray-100">
-      <Link to="/">
-        <div className="absolute font-bold top-4 left-4 text-black text-2xl border border-solid border-gray-300 rounded-full p-1">
-          <IoIosClose />
-        </div>
-      </Link>
-      <div className="px-5 py-16">
-        <DetailsSummary
-          airQuality={airQuality}
-          status={status}
-          error={error}
-          summary
-        />
+      <div className="flex justify-between items-center px-4 pt-3">
+        <Link to="/">
+          <div className="font-bold text-black text-2xl border border-solid border-gray-300 rounded-full p-1 w-8 h-8 flex flex-col justify-center items-center">
+            <IoIosClose />
+          </div>
+        </Link>
+        <Link to="/search">
+          <HiSearchCircle className="text-4xl text-gray-300 " />
+        </Link>
+      </div>
+      <div className="px-5 py-7">
+        <DetailsSummary airQuality={airQuality} status={status} error={error} summary />
         <Forcast airQuality={airQuality} />
         <ApComponents airQuality={airQuality} />
       </div>
